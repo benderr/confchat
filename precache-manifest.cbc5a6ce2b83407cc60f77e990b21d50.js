e604d7f32f18a269598c4aadedcccdce7955c6db
G.Ilshat <gaifullin.ilshat@gmail.com>
2018-10-09 23:25:58 +0500

    Updates

---
+++ precache-manifest.cbc5a6ce2b83407cc60f77e990b21d50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/confchat/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ced70b6720ec5a99086a",
+    "url": "/confchat/static/js/runtime~main.ced70b67.js"
+  },
+  {
+    "revision": "2363aee862b15e6e1f73",
+    "url": "/confchat/static/js/main.2363aee8.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/confchat/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "2363aee862b15e6e1f73",
+    "url": "/confchat/static/css/main.c8a994fc.chunk.css"
+  },
+  {
+    "revision": "16b8ea9a1439fee7ed214e36b77d12c0",
+    "url": "/confchat/index.html"
+  }
+];
