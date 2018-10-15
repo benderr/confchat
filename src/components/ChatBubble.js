2027ba599ecfcd9b6b2d4a8d7d30d3b34d33bfb8
G.Ilshat <gaifullin.ilshat@gmail.com>
2018-10-16 01:47:44 +0500

    chat list

---
+++ src/components/ChatBubble.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export const ChatBubble = ({isMy, date, from, text}) => {
+    const clearClass = !isMy ? 'clearfix' : '';
+    const messageClass = !isMy ? 'other-message float-right' : ' my-message';
+    return (
+        <li className={clearClass}>
+            {isMy && <TitleAuthor from={from} date={date}/>}
+            {!isMy && <TitleOther from={from} date={date}/>}
+            <div className={'message ' + messageClass}>
+                {text}
+            </div>
+        </li>
+    );
+};
+
+const TitleAuthor = ({isMy, from, date}) => {
+    return (<div className='message-data'>
+            <span className="message-data-name"> <i className="fa fa-circle online"></i> {from}</span>
+            <span className="message-data-time">{date.toLocaleString()}</span>
+        </div>
+    );
+};
+
+const TitleOther = ({isMy, from, date}) => {
+    return (<div className='message-data  align-right'>
+            <span className="message-data-time">{date.toLocaleString()}</span>
+            <span className="message-data-name">&nbsp;{from}</span>
+            &nbsp;<i className="fa fa-circle me"></i>
+        </div>
+    );
+};
